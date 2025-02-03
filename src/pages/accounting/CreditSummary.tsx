79b91f122b42898b18a44ec66eab1d53281ea2bd
tristan-gscn <tristan.gscn@gmail.com>
2025-02-03 20:36:42 +0100

    Debugging sqlite 2 (to be continued)

--- src/pages/accounting/CreditSummary.tsx
+++ src/pages/accounting/CreditSummary.tsx
@@ -2,12 +2,11 @@ import { useTranslation } from "react-i18next";
 import { CreditSummaryTR, CreditSummaryTH } from "../../components/credit-summary/CreditSummaryRow";
 import { CreditSummaryProperty } from "../../types/SummaryProperties";
 import FilterSelection from "../../components/filter-selection/FilterSelection";
-import DBUtils from "../../utils/database";
+
 
 function CreditSummary() {
 
     const { t } = useTranslation();
-    const one = DBUtils.getOne();
 
     return (
         <>
@@ -27,7 +26,7 @@ function CreditSummary() {
             </div>
             <table className="w-full table-auto border-white dark:border-gray-950 border-2 border-y-0 cursor-copy mt-40">
                 <tbody>
-                    <CreditSummaryTR content={one.toString()} />
+                    <CreditSummaryTR content={"A"} />
                     <CreditSummaryTR content={"B"} />
                     <CreditSummaryTR content={"C"} />
                     <CreditSummaryTR content={"D"} />
